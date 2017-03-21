8cf5d33d66995506ce08c7e29206b1684a7189c8
nitinbhide <nitinbhide@gmail.com>
2017-03-22 01:03:58 +0530

    added comments.

--- testdata/firstproject/upload/models.py
+++ testdata/firstproject/upload/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+#[NITIN] Do not create user model. Django has default user table with neccessary functions.
+#  we can extend user model.
 class User_table(models.Model):
     username_text = models.CharField(max_length=100)
     password_text = models.CharField(max_length=100)
@@ -10,7 +12,8 @@ class User_table(models.Model):
     no_of_tamper = models.IntegerField()
     client_ip = models.GenericIPAddressField(protocol='both', unpack_ipv4=True)
 
-
+#[NITIN] Do not name tables as '_DB' or '_Table'.  
+#  add some small comments about purpose of model. It is very confusing to understand what do you mean by 'Original'
 class Original_DB(models.Model):
     roll_no = models.IntegerField()
     name = models.CharField(max_length=100)
@@ -19,6 +22,8 @@ class Original_DB(models.Model):
     sec2 = models.IntegerField()
     sec3 = models.IntegerField()
     total = models.IntegerField()
+
+# [NITIN] I think AnswersheetScan will be a better model name. Hash value will be just one field of the AnswersheetScan
 
 class Hash_table(models.Model):
     hash_code =  models.CharField(max_length=100)
