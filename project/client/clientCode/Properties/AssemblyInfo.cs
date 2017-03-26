fa2529531f93ee5fca8ff55f0571e606ed0729dd
nitinbhide <nitinbhide@gmail.com>
2017-03-26 22:26:02 +0530

    updated assembly info with names.

---
+++ project/client/clientCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("X-Gen OMR Client")]
+[assembly: AssemblyDescription("OMR sheet uploads for X-Gen")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("X-Gen Smart India Hackathon Team")]
+[assembly: AssemblyProduct("X-Gen OMR Management")]
+[assembly: AssemblyCopyright("X-Gen Smart India Hackathon Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8df97661-c360-4ccf-9be1-eba4b3851bb1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-IN")]
