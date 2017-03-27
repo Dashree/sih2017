e171bc6606190809a8bc20788ed0f3b32caf7d70
Akshata Jahagirdar <akshata.jahagirdar@cumminscollege.in>
2017-03-27 13:53:23 +0530

    exam tables updated

--- project/server/djangoapp/exam/models.py
+++ project/server/djangoapp/exam/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 #This table contains the information regarding the Exam information.
 class Exam_table(models.Model):
-    exam_id = models.CharField(max_length=100)
+    examid = models.CharField(max_length=100)
     exam_name = models.CharField(max_length=100)
     #exam_date = models.datefield(null = 'True')
 
-#This table contains the basic and orignal student information.
+#This table contains the basic and original student information.
 class Student_info(models.Model):
     Roll_no = models.CharField(max_length=10)
     student_name = models.CharField(max_length=100)
@@ -15,9 +15,11 @@ class Student_info(models.Model):
 
 #[Akshata this table will hold the marks when they come from the workers.]
 class Final_marks(models.Model):
-    sect1 = models.IntegerField(null = 'True')
-    sect2 = models.IntegerField(null = 'True')
-    sect3 = models.IntegerField(null = 'True')
-    total = models.IntegerField(null = 'True')
+    examid = models.ForeignKey('Exam_table', on_delete=models.CASCADE)
+    studentid = models.ForeignKey('Student_info', on_delete=models.CASCADE)
+    sect_no = models.IntegerField()
+    ques_no = models.IntegerField()
+    mark = models.IntegerField()
+    
     
     
--- project/server/djangoapp/exam/views.py
+++ project/server/djangoapp/exam/views.py
@@ -5,12 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Exams app")
 
-class session_info(request):
-    
-    def get_client_ip(request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
-        else:
-            ip = request.META.get('REMOTE_ADDR')
-        return ip
+
+
