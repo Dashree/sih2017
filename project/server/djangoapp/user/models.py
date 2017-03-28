45815f7d6d122e6fa053941ee115bfaad15a137a
Akshata Jahagirdar <akshata.jahagirdar@cumminscollege.in>
2017-03-28 17:10:41 +0530

    hash table in user commented as same written in upload

--- project/server/djangoapp/user/models.py
+++ project/server/djangoapp/user/models.py
@@ -6,12 +6,12 @@ from django.db import models
 # [NITIN] I think AnswersheetScan will be a better model name. Hash value will be just one field of the AnswersheetScan
 
    
-class Hash_table(models.Model):
-    hash_code =  models.CharField(max_length=100)
-    img_addr = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.img_addr
-
-    
-
+##class Hash_table(models.Model):
+##    hash_code =  models.CharField(max_length=100)
+##    img_addr = models.CharField(max_length=100)
+##
+##    def __str__(self):
+##        return self.img_addr
+##
+##    
+##
