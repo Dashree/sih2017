9b6a0379ce1c302cd36425f88f5aea6f15af9577
lavina agarwal <lavina.agarwal@cumminscollege.in>
2017-03-29 18:15:45 +0530

    Hash code byte array converted to string

--- project/client/clientCode/LogIn.cs
+++ project/client/clientCode/LogIn.cs
@@ -15,7 +15,8 @@ namespace client
     {   
         string serverUrl;
         string loginUrl = "/user/login/";
-        
+     
+
         WebClient webclient = new WebClient();
         public LogIn(string serverUrl)
         {
@@ -26,34 +27,41 @@ namespace client
 
         private bool login(string username, string password)
         {
+            MessageBox.Show(username);
+            MessageBox.Show(password);
             NameValueCollection login_params = new NameValueCollection();
             login_params.Add("username", username);
             login_params.Add("password", password);
-
-            bool logged_in = true;
+            bool logged_in = false;
             try
             {
                 byte[] responseArray = this.webclient.UploadValues(this.loginUrl, login_params);
+                //string response = System.Text.Encoding.UTF8.GetString(serverResponse); 
             }
-            catch(WebException exp)
+            catch (WebException exp)
             {
                 HttpWebResponse response = (System.Net.HttpWebResponse)exp.Response;
-                 if (response.StatusCode != HttpStatusCode.OK)
-                 {
-                     logged_in = false;
-                 }
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    logged_in = false;
+                }
             }
             return logged_in;
         }
-        
+
 
         private void loginBtn_Click(object sender, EventArgs e)
         {
-            if (login(userNameBox.Text, this.passwordBox.Text) == true)
+           if (login(userNameBox.Text, this.passwordBox.Text) == true)
             {
+                label3.Visible = false;
                 this.Hide();
                 FileUpload upload = new FileUpload(this.serverUrl);
                 upload.Show();
+            }
+            else
+            {
+                label3.Visible = true;
             }
         }
     }
