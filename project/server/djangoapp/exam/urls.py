7476ad38f47f90093a2083a461f5b09a86a5c28f
ShreedaSG <ShreedaSG@localhost>
2017-04-02 02:14:00 +0530

    worker download images urls added.

--- project/server/djangoapp/exam/urls.py
+++ project/server/djangoapp/exam/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
+from .views import download_rollnumbersect, download_centrecodesect, download_answercolumnsect
+
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<examid>\w+)/template/rollnosect$', download_rollnumbersect, name='download_rollnumbersect'),
+    url(r'^(?P<examid>\w+)/template/centrecodesect$', download_centrecodesect, name='centrecodedownload'),
+    url(r'^(?P<examid>\w+)/template/answercolumnsect$', download_answercolumnsect, name='answercolumndownload'),
+    #url(r'^(?P<examid>\w+)/template/completetemplate$', download_completetemplate, name='completetemplatedownload'),
 ]
