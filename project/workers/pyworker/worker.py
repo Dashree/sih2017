63bb3bd7556e2ed8925dbd0a6b2790a23449c3a6
lavina agarwal <lavina.agarwal@cumminscollege.in>
2017-04-02 16:49:07 +0530

    worker function calls fixed for image download.

--- project/workers/pyworker/worker.py
+++ project/workers/pyworker/worker.py
@@ -3,24 +3,27 @@ import subprocess
 import shutil
 import requests
 import tempfile
+import re
+    
 from hueyconfig import huey  # import our "huey" object
 
 ANSWERPAPER_URL = "/upload/image/%s/"
-OCTAVE_PATH = "C:/ProgramFiles(x86)/Notepad++/notepad++.exe"
+OCTAVE_PATH = "C:/temp"
 
 #@huey.task()
 def detect(host_name, examid, studentid, scannedimageid):
+    print("downloading mage")
     model_answer_url = "exam/%s/download/modelanswer/" % examid
-    student_answer_sheet =  r"^upload/image/%s/" % scannedimageid
     url = host_name + (ANSWERPAPER_URL %scannedimageid)
-    model_answer_template = download_image(url)
+    scanned_image_path = download_image(url)
+    print(scanned_image_path)
     #model_answer_template = r'C:\Python27\OMR_Templates\Template1\ModelAns.jpg'
     
     os.chdir(OCTAVE_PATH)
     #process = subprocess.Popen(['octave.exe',r'C:\Users\Admin-PC\Desktop\impython.m', 'shruti'], stdout=subprocess.PIPE)
-    process.wait()
-    marks = process.stdout.read()
-    send_marks(host_name, examid, studentid, scannedimageid, marks)
+    #process.wait()
+    #marks = process.stdout.read()
+    #send_marks(host_name, examid, studentid, scannedimageid, marks)
     #delete downloaded images.
 
 @huey.task()
@@ -36,7 +39,9 @@ def download_image(url):
     print(url)
     #url = 'http://www.axiome.ch/typo3temp/pics/14c77f8f8a.jpg'
     response = requests.get(url, stream=True)
-    filename = response.filename
+    d = response.headers['content-disposition']
+    filename = re.findall("filename=(.+)", d)[0]
+
     tempjpgpath = tempfile.gettempdir()
     tempjpgpath = os.path.join(tempjpgpath, filename)
     with open(tempjpgpath, "wb") as fjpg:
@@ -44,5 +49,5 @@ def download_image(url):
     return tempjpgpath
 
 if __name__  == '__main__':
-    detect('http://localhost:8000',2,1,1)
+    detect('http://localhost:8000',2,1,2)
 
