5c154efa8c4d22648f23a3f65a3918b805726358
ShreedaSG <ShreedaSG@localhost>
2017-04-02 12:24:06 +0530

    added worker folder in python package search path.

--- project/server/djangoapp/firstproject/settings.py
+++ project/server/djangoapp/firstproject/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import site
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+workerdir = os.path.normpath(os.path.join(BASE_DIR, '..', '..', 'worker'))
+site.addsitedir(workerdir)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
