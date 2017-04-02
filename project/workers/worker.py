d2bba7d9f441d7eacbd63e534f8ac1392b6348d6
lavina agarwal <lavina.agarwal@cumminscollege.in>
2017-04-02 14:17:57 +0530

    minor changes. not working yet.

--- project/workers/worker.py
+++ project/workers/worker.py
@@ -3,17 +3,17 @@ import subprocess
 import shutil
 import requests
 import tempfile
-from config import huey  # import our "huey" object
+from hueyconfig import huey  # import our "huey" object
 
 ANSWERPAPER_URL = "/%s/"
 OCTAVE_PATH = "C:/ProgramFiles(x86)/Notepad++/notepad++.exe"
 
-@huey.task()
+#@huey.task()
 def detect(host_name, examid, studentid, answerpaperid):
     model_answer_url = "exam/%s/download/modelanswer/" % examid
     student_answer_sheet =  r"^upload/image/%s/" % imageid
     url = host_name + (ANSWERPAPER_URL % answerpaperid)
-    model_answer_template = download_image(url, 'ans%s" % answerpaperid)
+    model_answer_template = download_image(url, "ans%s" % answerpaperid)
     #model_answer_template = r'C:\Python27\OMR_Templates\Template1\ModelAns.jpg'
     
     os.chdir(OCTAVE_PATH)
@@ -21,6 +21,11 @@ def detect(host_name, examid, studentid, answerpaperid):
     process.wait()
     marks = process.stdout.read()
     send_marks(host_name, examid, studentid, answerpaperid, marks)
-	File.delete(save=True)
     #delete downloaded images.
+
+@huey.task()
+def worker_test(host_name, examid):
+    print(host_name)
+    print(examid)
+
 
