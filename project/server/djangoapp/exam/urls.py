c80a906a9c984625ddf34842d40dcf837b0d607e
Akshata Jahagirdar <akshata.jahagirdar@cumminscollege.in>
2017-04-02 15:55:38 +0530

    not required urls anymore

--- project/server/djangoapp/exam/urls.py
+++ project/server/djangoapp/exam/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import url
-from upload.views import download_rollnumbersect, download_centrecodesect, download_answercolumnsect,download_completetemplate
-
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<examid>\w+)/template/rollnosect$', download_rollnumbersect, name='download_rollnumbersect'),
-    url(r'^(?P<examid>\w+)/template/centrecodesect$', download_centrecodesect, name='centrecodedownload'),
-    url(r'^(?P<examid>\w+)/template/answercolumnsect$', download_answercolumnsect, name='answercolumndownload'),
-    url(r'^(?P<examid>\w+)/template/completetemplate$', download_completetemplate, name='completetemplatedownload'),
+   # url(r'^(?P<examid>\w+)/template/rollnosect$', download_rollnumbersect, name='download_rollnumbersect'),
+   # url(r'^(?P<examid>\w+)/template/centrecodesect$', download_centrecodesect, name='centrecodedownload'),
+   # url(r'^(?P<examid>\w+)/template/answercolumnsect$', download_answercolumnsect, name='answercolumndownload'),
+   # url(r'^(?P<examid>\w+)/template/completetemplate$', download_completetemplate, name='completetemplatedownload'),
 ]
